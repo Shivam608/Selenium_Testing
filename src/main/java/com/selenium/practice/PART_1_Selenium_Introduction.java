ba747366384f8ab63c7f3748ce709661eee1cdff
PranayShivam <pronoybose7@gmail.com>
2025-07-11 08:49:10 +0530

    Updated Code base

--- src/main/java/com/selenium/practice/PART_1_Selenium_Introduction.java
+++ src/main/java/com/selenium/practice/PART_1_Selenium_Introduction.java
@@ -1,17 +1,27 @@
 package com.selenium.practice;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class PART_1_Selenium_Introduction {
     public static void main(String[] args) {
 
         //Invoking Browser
         //Chrome - ChromeDriver --> Methods
-        System.setProperty("webdriver.chorme.driver", "\"C:\\Users\\prono\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe\"");
-        ChromeDriver driver = new ChromeDriver();
+//        System.setProperty("webdriver.chorme.driver", "\"C:\\Users\\prono\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe\"");
+//        ChromeDriver driver = new ChromeDriver();
+
+        //FireFox - geckodriver --> Methods
+//        FirefoxDriver driver = new FirefoxDriver();
+
+        //Edge Browser -> msedgedriver -> Methods
+        EdgeDriver driver = new EdgeDriver();
+
         driver.get("https://www.amazon.in/");
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getPageSource());
         driver.close();  //driver.quit();
 
     }
